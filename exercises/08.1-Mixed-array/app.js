28606aff13824683623cc42c1fe901ff9db9d8b1
Edwin Moreno <edwinmoreno77@gmail.com>
2024-07-31 12:22:38 -0400

    mixed-array finished

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,6 @@
-let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
+let mix = [42, true, "towel", [2, 1], 'hello', 34.4, { "name": "juan" }];
 
 // Your code here
+let arr = [];
+for (data of mix) arr.push(typeof data);
+console.log(arr);
