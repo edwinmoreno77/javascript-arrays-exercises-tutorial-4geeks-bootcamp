f02978260cfe661dee3b8ba4f9b61ffccbd8bec7
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-02 14:52:14 -0400

    Do while finished

--- exercises/11-Do-while/app.js
+++ exercises/11-Do-while/app.js
@@ -2,6 +2,11 @@ let i = 20;
 
 do {
     // Magic goes here
+    if (i === 0) {
+        console.log('LIFTOFF');
+    } else {
+        (i % 5 === 0) ? console.log(i + '!') : console.log(i);
+    }
 
     i--;
 } while (i >= 0);
