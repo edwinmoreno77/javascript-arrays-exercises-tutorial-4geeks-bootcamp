ad6890f10698bc2e656563cf185b0846194302d1
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-03 17:46:27 -0400

    12-delete-element

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -1,6 +1,9 @@
-let people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
+let people = ['juan', 'ana', 'michelle', 'daniella', 'stefany', 'lucy', 'barak', 'emilio'];
 
 // Your code below
+function deletePerson(name) {
+    return people.filter((element) => element !== name)
+}
 
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
