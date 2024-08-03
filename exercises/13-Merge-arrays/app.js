0e6697c46c12f616952c3c32625552e782b5e2fb
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-03 17:47:30 -0400

    Merge-arrays

--- exercises/13-Merge-arrays/app.js
+++ exercises/13-Merge-arrays/app.js
@@ -1,9 +1,10 @@
-let chunkOne = [ 'Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell' ];
-let chunkTwo = [ 'Lucas', 'Jake', 'Scott', 'Amy', 'Molly', 'Hannah', 'Lucas' ];
+let chunkOne = ['Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell'];
+let chunkTwo = ['Lucas', 'Jake', 'Scott', 'Amy', 'Molly', 'Hannah', 'Lucas'];
 
 const mergeArrays = (firstArray, secondArray) => {
     let newArray = []
     // Your code here
+    newArray = firstArray.concat(secondArray)
     return newArray
 }
 
