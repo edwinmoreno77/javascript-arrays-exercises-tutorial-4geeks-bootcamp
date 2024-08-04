ff045081e8e70d89bfc2c87e36b77ce4c2b7d606
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-04 16:34:23 -0400

    divide-amd-conquer finished

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,3 +1,15 @@
 let listOfNumbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68];
 
 // Your code here
+function mergeTwoList(array) {
+
+    let odd = [];
+    let even = [];
+
+    for (num of array) (num % 2 !== 0) ? odd.push(num) : even.push(num);
+
+    return odd.concat(even);
+
+}
+
+console.log(mergeTwoList(listOfNumbers));
