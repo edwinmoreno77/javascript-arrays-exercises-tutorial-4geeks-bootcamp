eb15cd2e0f3c375bdff34cc06006708b9d793532
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-09 15:43:11 -0400

    map and array finished

--- exercises/20.1-Map-an-array/app.js
+++ exercises/20.1-Map-an-array/app.js
@@ -1,9 +1,10 @@
 let arrayOfCelsiusValues = [-2, 34, 56, -10];
 
-let arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value) {
-	
+let arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function (value) {
+
 	// Add your code here and return the new value
-	
+	return (value * 9 / 5) + 32
+
 });
 
 console.log(arrayOfFahrenheitValues);
