2d9227309c760bccd978e826581d60b9279b9671
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-10 16:52:04 -0400

    amp with function inside variable finished

--- exercises/20.3-Map-with-function-inside-variable/app.js
+++ exercises/20.3-Map-with-function-inside-variable/app.js
@@ -1,7 +1,12 @@
 let names = ['Alice', 'Bob', 'Marry', 'Joe', 'Hilary', 'Stevia', 'Dylan'];
 
-let prepender = function(name) {
-	return 'My name is: '+name;
+let prepender = function (name) {
+	return 'My name is: ' + name;
 };
 
 // Your code here
+
+let result = names.map(prepender);
+
+console.log(result);
+
