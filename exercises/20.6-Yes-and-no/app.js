b50d6e8ffe3215aa824273bd377e21ead495899c
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-11 10:52:15 -0400

    yes and no finished

--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,10 @@
 let theBools = [0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 
 // Your code here
+let newBools = theBools.map((item) => {
+    if (item == 1) return 'wiki';
+    if (item == 0) return 'woko';
+});
+
+console.log(newBools);
+
