279043928feb646536bee889514a85d1a3c77ce4
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-11 10:51:07 -0400

    map arrays of objects finished

--- exercises/20.5-Map-arrays-of-objects/app.js
+++ exercises/20.5-Map-arrays-of-objects/app.js
@@ -1,14 +1,25 @@
 let people = [
-	{ name: 'Joe', birthDate: new Date(1986,10,24) },
-	{ name: 'Bob', birthDate: new Date(1975,5,24) },
-	{ name: 'Erika', birthDate: new Date(1989,6,12) },
-	{ name: 'Dylan', birthDate: new Date(1999,12,14) },
-	{ name: 'Steve', birthDate: new Date(2003,4,24) }
+	{ name: 'Joe', birthDate: new Date(1986, 10, 24) },
+	{ name: 'Bob', birthDate: new Date(1975, 5, 24) },
+	{ name: 'Erika', birthDate: new Date(1989, 6, 12) },
+	{ name: 'Dylan', birthDate: new Date(1999, 12, 14) },
+	{ name: 'Steve', birthDate: new Date(2003, 4, 24) }
 ];
 
-let simplifier = function(person) {
+let simplifier = function (person) {
 	// Your code here
-	return person.name;
+	let dateCurrent = new Date();
+	let personAge = dateCurrent.getFullYear() - person.birthDate.getFullYear();
+	let birthDateInTheYear = new Date(dateCurrent.getFullYear(), person.birthDate.getMonth(), person.birthDate.getDate());
+
+	if (dateCurrent < birthDateInTheYear) {
+		personAge = personAge - 1
+	}
+
+	return `Hello, my name is ${person.name} and I am ${personAge} years old`;
 };
 
+
 console.log(people.map(simplifier));
+
+
