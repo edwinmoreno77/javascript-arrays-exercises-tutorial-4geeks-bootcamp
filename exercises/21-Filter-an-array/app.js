ea4550b6554820aed17eebe4e856f8a3b2d6f122
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-12 11:12:47 -0400

    filter an-array finished

--- exercises/21-Filter-an-array/app.js
+++ exercises/21-Filter-an-array/app.js
@@ -1,5 +1,6 @@
 let allNames = ["Romario", "Boby", "Roosevelt", "Emiliy", "Michael", "Greta", "Patricia", "Danzalee"];
 
 // Your code here
+let resultingNames = allNames.filter((name) => name[0].toLowerCase() == 'r');
 
 console.log(resultingNames);
