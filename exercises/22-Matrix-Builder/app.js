409b0cc7fb166fc9aa23850743960dafb573a7e3
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-14 17:22:25 -0400

    Matrix builder finished

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,6 +1,16 @@
 // Your code here
 
+function matrixBuilder(number) {
+    let result = [];
 
-
+    for (let i = 0; i < number; i++) {
+        let array = [];
+        for (let j = 0; j < number; j++) {
+            array.push(Math.floor(Math.random() * 2));
+        }
+        result.push(array);
+    }
+    return result;
+}
 // Do not change anything from this line down
-console.log(matrixBuilder(5))
+console.log(matrixBuilder(5));
