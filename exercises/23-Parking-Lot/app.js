8e3b7d976456a0db0a7ffc53282b77d0724938aa
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-16 15:07:09 -0400

    parking lot

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -6,6 +6,20 @@ let parkingState = [
 ]
 
 // Your code here
+function getParkingLotState(parking) {
+  let state = {
+    totalSlots: 0,
+    availableSlots: 0,
+    occupiedSlots: 0,
+  }
+  for (let arraySlot of parking) {
+    for (let slot of arraySlot) {
+      if (slot == 2) state.availableSlots += 1;
+      if (slot == 1) state.occupiedSlots += 1;
+    }
+  }
+  state.totalSlots = state.availableSlots + state.occupiedSlots;
+  return state;
+}
 
-
-console.log(getParkingLotState(parkingState))
+console.log(getParkingLotState(parkingState));
