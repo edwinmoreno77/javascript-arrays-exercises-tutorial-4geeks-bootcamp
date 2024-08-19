0b0e598812c9b3ca9d9789d25212846ee00f70d7
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-19 17:05:13 -0400

    making a ul finished

--- exercises/24-Making-a-ul/app.js
+++ exercises/24-Making-a-ul/app.js
@@ -1,28 +1,31 @@
 let allColors = [
-	{label: 'Red', sexy: true},
-	{label: 'Pink', sexy: false},
-	{label: 'Orange', sexy: true},
-	{label: 'Brown', sexy: false},
-	{label: 'Pink', sexy: true},
-	{label: 'Violet', sexy: true},
-	{label: 'Purple', sexy: false},
+	{ label: 'Red', sexy: true },
+	{ label: 'Pink', sexy: false },
+	{ label: 'Orange', sexy: true },
+	{ label: 'Brown', sexy: false },
+	{ label: 'Pink', sexy: true },
+	{ label: 'Violet', sexy: true },
+	{ label: 'Purple', sexy: false },
 ];
 
 function generateLI(color) {
 	// Your code here
+	return `<li>${color.label}</li>`
 }
 
 function filterColors(color) {
 	// Your code here
+	return color.sexy == true
 }
 
 function generateHTMLFromArray(array) {
-	
+
 	let filteredOptions = array.filter((filterColors));
+
 	let LIs = filteredOptions.map(generateLI);
 
 	let htmlString = '<ul>';
-	LIs.forEach(function(element) {
+	LIs.forEach(function (element) {
 		htmlString += element;
 	})
 	htmlString += '</ul>';
@@ -30,3 +33,5 @@ function generateHTMLFromArray(array) {
 }
 
 console.log(generateHTMLFromArray(allColors));
+
+
