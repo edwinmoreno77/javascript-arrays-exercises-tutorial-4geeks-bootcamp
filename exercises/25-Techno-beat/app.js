9792fb8e029d54bd6a90805b0b6ae62f13e7bb7b
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-21 16:19:36 -0400

    tachno beat finished

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,9 +1,27 @@
 // Your code here
+function lyricsGenerator(array) {
+
+    let string = "";
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] == 0) {
+            string += 'Boom ';
+        } else if (array[i] == 1) {
+            string += 'Drop the bass ';
+        }
+
+        if (array[i - 2] == 1 && array[i - 1] == 1 && array[i] == 1) string += '!!!Break the bass!!! '
+
+    }
+    return string;
+}
 
 
 // Don't change anything below this line
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+console.log(lyricsGenerator([0, 0, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 1, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 0]))
+console.log(lyricsGenerator([1, 0, 1]))
+console.log(lyricsGenerator([1, 1, 1]))
+
